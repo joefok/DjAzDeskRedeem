e5d5aae4fbdfbdc647e2c70d03b45efc4cd0baee
joefo <g6>
2021-11-28 22:45:20 +0800

    ok

--- app/upload/models.py
+++ app/upload/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Register(models.Model):
+    token = models.CharField(max_length=255)
+    assign_seat = models.CharField(max_length=5)
+    pass
