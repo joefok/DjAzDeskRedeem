809eccd9a154f877bb1f5640f5bb5c7caaaa9be0
joefo <g6>
2021-11-28 11:52:25 +0800

    ok

--- app/upload/views.py
+++ app/upload/views.py
@@ -3,15 +3,19 @@ from django.core.files.storage import FileSystemStorage
 # from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 import csv
+token = {}
 with open('upload/mac_map.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
         print(', '.join(row))
+        token[row[0]] = row[-1]
 
 base_url = 'https://guatraroom.eastasia.cloudapp.azure.com/guacamole/?username=secret&password=secret'
 
 
 def image_upload(request):
+    if request.method == 'GET':
+        print(request.GET)
     if request.method == "POST" and request.FILES["image_file"]:
         image_file = request.FILES["image_file"]
         fs = FileSystemStorage()
