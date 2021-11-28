ea67e299f03caf34adb2e29b3cdac8ca1af601fe
joefo <g6>
2021-11-28 12:25:52 +0800

    ok

--- app/upload/views.py
+++ app/upload/views.py
@@ -4,11 +4,13 @@ from django.core.files.storage import FileSystemStorage
 from django.shortcuts import redirect
 import csv
 token = {}
+counter = 101
 with open('upload/mac_map.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         print(', '.join(row))
-        token[row[0]] = row[-1]
+        token[row[0]] = counter
+        counter += 1
 print(token)
 base_url = 'https://guatraroom.eastasia.cloudapp.azure.com/guacamole/?username=secret&password=secret'
 
@@ -17,6 +19,6 @@ def image_upload(request):
     token_get = request.GET.get('token')
     if token_get is None:
         return redirect(base_url)
-    return redirect(base_url.replace('secret', '{}'.format(token[token_get]).replace('-','R0')))
+    return redirect(base_url.replace('secret', '1R{}'.format(token[token_get])))
     # return render(request, "upload.html")
 
